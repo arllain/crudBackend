be88096f260f9528666bce245c2070795a9949fe
arllain <arllain@gmail.com>
2018-11-15 07:41:32 -0300

    Criação das classes da camada de acesso a dados(Repository)

---
+++ crudBackend/src/main/java/com/arllain/repositories/PessoaRepository.java
@@ -0,0 +1,12 @@
+package com.arllain.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.arllain.domain.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+}
